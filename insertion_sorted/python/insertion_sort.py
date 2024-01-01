98895eadbee82dba3ce61a8b68eed738e4567fc8
Thi <tthiagovinnicius@gmail.com>
2024-01-01 16:45:07 -0300

    refactor: Refatorando a função principal

--- insertion_sorted/python/insertion_sort.py
+++ insertion_sorted/python/insertion_sort.py
@@ -1,21 +1,25 @@
+import random
 
+# Função para a ordenação de uma lista com o algoritmo INSERTION SORTED
+def insertion_sorted(lista):
 
-def insertion_sort(lista):
-    # Percorre a lista a partir do segundo elemento até o final
-    for elemento in range(1, len(lista)):
-        # Armazena o valor do elemento atual da lista
-        valorAtual = lista[elemento] 
-        
-        # Define a posição do elemento anterior na lista
-        posicao = elemento - 1
-        
-        # Enquanto a posição for válida e o elemento na posição for maior que o valor atual
-        while posicao >= 0 and lista[posicao] > valorAtual:
-            # Move o elemento na posição para a direita na lista para abrir espaço para o valor atual
+    # Percorreno a lista apartir da segunda posição até a ultima.
+    for indice in range(1, len(lista)):
+        # Pegando o valor da posição atual da lista.
+        valor_atual = lista[indice]
+
+        # Definindo a posição do elemento anterior na lista.
+        posicao = indice - 1
+
+        while posicao >=0 and valor_atual < lista[posicao]:
             lista[posicao + 1] = lista[posicao]
-            
-            # Decrementa a posição para verificar o próximo elemento à esquerda
             posicao -= 1
 
-        # Insere o valor atual na posição correta na lista ordenada
-        lista[posicao + 1] = valorAtual
+        lista[posicao + 1] = valor_atual
+
+
+if __name__ == "__main__":
+    dados = [i for i in range(1000)]
+    dados = random.sample(dados, 100)
+
+    print(dados)
