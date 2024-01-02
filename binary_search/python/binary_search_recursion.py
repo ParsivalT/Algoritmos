232e91146b96f9d2caea2ad0f85a908b42aad97c
Thi <tthiagovinnicius@gmail.com>
2024-01-02 20:03:05 -0300

    refactor: Melhorando o codigo visualmente.

--- binary_search/python/binary_search_recursion.py
+++ binary_search/python/binary_search_recursion.py
@@ -1,5 +1,5 @@
 # Função que realiza a busca binária de forma recursiva em uma lista ordenada.
-def binary_search(list: list, start: int, end: int, value: int) -> int:
+def binary_search_recursion(list: list, start: int, end: int, value: int) -> int:
     # Retorna -1 caso o valor procurado não esteja presente na lista.
     if start > end:
         return -1 
@@ -15,16 +15,16 @@ def binary_search(list: list, start: int, end: int, value: int) -> int:
     # Chama recursivamente a função com uma nova faixa à esquerda do índice do meio.
     # Isso é feito se o valor atual for maior que o valor procurado.
     if current_value > value:
-        return binary_search(list=list, start=start, end=mid - 1, value=value)
+        return binary_search_recursion(list=list, start=start, end=mid - 1, value=value)
     
     # Chama recursivamente a função com uma nova faixa à direita do índice do meio.
     # Isso é feito se o valor atual for menor que o valor procurado.
     if current_value < value:
-        return binary_search(list=list, start=mid + 1, end=end, value=value)
+        return binary_search_recursion(list=list, start=mid + 1, end=end, value=value)
     
 
 if __name__ == "__main__":  
     data = [2, 4, 5, 6, 8, 9, 11, 15, 17]
 
     # Exemplo de uso da função de busca binária.
-    print(binary_search(data, 0, len(data) - 1, 8))
+    print(binary_search_recursion(data, 0, len(data) - 1, 8))
